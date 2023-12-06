2500b53b84f1ba0f03b00f9eb15975cddf4edbd9
Lokesh <lallytanwar@gmail.com>
2023-12-07 00:46:44 +0530

    some minor changes

--- version_3_final_code.ino
+++ version_3_final_code.ino
@@ -1,6 +1,6 @@
-#include <Adafruit_BNO055.h>  // For BNO055 IMU
-#include <Adafruit_BMP3XX.h>  // For BMP388 Altimeter
-#include <Servo.h>            // For controlling servo
+#include <Adafruit_BNO055.h> // For BNO055 IMU
+#include <Adafruit_BMP3XX.h> // For BMP388 Altimeter
+#include <Servo.h>           // For controlling servo
 #include <Wire.h>
 #include <TinyGPS++.h>
 #include <SPI.h>
@@ -9,18 +9,36 @@ unsigned long starxbee = 0;
 unsigned long starbno = 0;
 #define SEALEVELPRESSURE_HPA (1013.25)
 // first 17 bits of xbee data with address start delimeter etc
-byte packet[2500] = {0x7E, 0x00, 0x0E, 0x10, 0x01, 0x00, 0x13, 0xA2, 0x00, 0x41, 0xF4, 0x25, 0xE8, 0xFF, 0xFE, 0x00, 0x00,};
-//end 
-//Tiny gps for gps data parsing
+byte packet[2500] = {
+    0x7E,
+    0x00,
+    0x0E,
+    0x10,
+    0x01,
+    0x00,
+    0x13,
+    0xA2,
+    0x00,
+    0x41,
+    0xF4,
+    0x25,
+    0xE8,
+    0xFF,
+    0xFE,
+    0x00,
+    0x00,
+};
+// end
+// Tiny gps for gps data parsing
 TinyGPSPlus gps;
 Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28); // BNO sensor data
-Adafruit_BMP3XX bmp; //BMP library initialization
-
+Adafruit_BMP3XX bmp;                             // BMP library initialization
 
 int D2 = 8;
 //---------------------------------------- Custom data types sending ------------------------------------------------//
 // Define a custom struct to store GNSS data
-struct GNSSData {
+struct GNSSData
+{
   long time;
   float speed;
   double latitude;
@@ -30,17 +48,20 @@ struct GNSSData {
 };
 
 // Define a custom struct to store voltage sensor data
-struct VoltageData {
+struct VoltageData
+{
   float voltage;
 };
 
 // Define a custom struct to store vibration sensor data
-struct VibrationData {
+struct VibrationData
+{
   int vibration;
 };
 
 // Define a custom struct to store BNO055 sensor data (9 DOF)
-struct BNO055Data {
+struct BNO055Data
+{
   float eulerX;
   float eulerY;
   float eulerZ;
@@ -50,10 +71,10 @@ struct BNO055Data {
   float gyroX;
   float gyroY;
   float gyroZ;
-  float temperature; // Temperature in degrees Celsius
-  float magX;        // Magnetic field strength X (uT)
-  float magY;        // Magnetic field strength Y (uT)
-  float magZ;        // Magnetic field strength Z (uT)
+  float temperature;  // Temperature in degrees Celsius
+  float magX;         // Magnetic field strength X (uT)
+  float magY;         // Magnetic field strength Y (uT)
+  float magZ;         // Magnetic field strength Z (uT)
   float linearAccelX; // Linear acceleration X (m/s^2)
   float linearAccelY; // Linear acceleration Y (m/s^2)
   float linearAccelZ; // Linear acceleration Z (m/s^2)
@@ -65,18 +86,22 @@ struct BNO055Data {
 
 //---------------------------------------- GNSS data read types sending  ------------------------------------------------//
 // Function to read and parse data from the GNSS sensor
-struct GNSSData readGNSSData(TinyGPSPlus &gps) {
+struct GNSSData readGNSSData(TinyGPSPlus &gps)
+{
   GNSSData data;
 
   // Check if GPS data is available
-  if (gps.location.isUpdated()) {
+  if (gps.location.isUpdated())
+  {
     data.time = gps.time.value();
     data.speed = gps.speed.mps();
     data.latitude = gps.location.lat();
     data.longitude = gps.location.lng();
     data.altitude = gps.altitude.meters();
     data.satellites = gps.satellites.value();
-  } else {
+  }
+  else
+  {
     // Set default values if data is not available
     data.time = 0;
     data.speed = 0.0;
@@ -92,7 +117,8 @@ struct GNSSData readGNSSData(TinyGPSPlus &gps) {
 
 //---------------------------------------- Voltage data read types sending ----------------------------------------------//
 // Function to read data from the voltage sensor
-struct VoltageData readVoltageSensor() {
+struct VoltageData readVoltageSensor()
+{
   VoltageData data;
 
   // Read voltage from the sensor (you may need to adjust this based on your sensor)
@@ -104,7 +130,8 @@ struct VoltageData readVoltageSensor() {
 
 //---------------------------------------- Vibration data read types sending ---------------------------------------------//
 // Function to read data from the SW420 vibration sensor
-struct VibrationData readVibrationSensor() {
+struct VibrationData readVibrationSensor()
+{
   VibrationData data;
 
   // Read vibration sensor data (you may need to adjust this based on your sensor)
@@ -114,177 +141,183 @@ struct VibrationData readVibrationSensor() {
 }
 //---------------------------------------- vibration data read types sending end -------------------------------------------//
 
-
-
-
 //---------------------------------------- BNO data read types sending  ----------------------------------------------------//
 // Function to read data from the BNO055 sensor (9 DOF)
-struct BNO055Data readBNO055Sensor() {
+struct BNO055Data readBNO055Sensor()
+{
   sensors_event_t event;
   BNO055Data data;
 
   bno.getEvent(&event);
-
+  imu::Vector<3> meg = bno.getVector(Adafruit_BNO055::VECTOR_MAGNETOMETER);
+  imu::Vector<3> acc = bno.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
+  imu::Vector<3> gyroz = bno.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
   data.eulerX = event.orientation.x;
   data.eulerY = event.orientation.y;
   data.eulerZ = event.orientation.z;
   data.accelX = event.acceleration.x;
   data.accelY = event.acceleration.y;
   data.accelZ = event.acceleration.z;
-  data.gyroX = event.gyro.x;
-  data.gyroY = event.gyro.y;
-  data.gyroZ = event.gyro.z;
-  data.temperature = bno.getTemp(); // Read temperature from BMP388
-  data.magX = event.magnetic.x; // Magnetic field strength X (uT)
-  data.magY = event.magnetic.y; // Magnetic field strength Y (uT)
-  data.magZ = event.magnetic.z; // Magnetic field strength Z (uT)
-  data.linearAccelX = event.acceleration.x; // Linear acceleration X (m/s^2)
-  data.linearAccelY = event.acceleration.y; // Linear acceleration Y (m/s^2)
-  data.linearAccelZ = event.acceleration.z; // Linear acceleration Z (m/s^2)
-  data.gravityX = event.acceleration.x; // Gravitational acceleration X (m/s^2)
-  data.gravityY = event.acceleration.y; // Gravitational acceleration Y (m/s^2)
-  data.gravityZ = event.acceleration.z; // Gravitational acceleration Z (m/s^2)
+  data.gyroX = gyroz.x();
+  data.gyroY = gyroz.y();
+  data.gyroZ = gyroz.z();
+  data.temperature = bno.getTemp();         // Read temperature from BMP388
+  data.magX = meg.x();             // Magnetic field strength X (uT)
+  data.magY = meg.y();             // Magnetic field strength Y (uT)
+  data.magZ = meg.z();             // Magnetic field strength Z (uT)
+  data.linearAccelX = acc.x(); // Linear acceleration X (m/s^2)
+  data.linearAccelY = acc.y(); // Linear acceleration Y (m/s^2)
+  data.linearAccelZ = acc.z(); // Linear acceleration Z (m/s^2)
+  data.gravityX = event.acceleration.x;     // Gravitational acceleration X (m/s^2)
+  data.gravityY = event.acceleration.y;     // Gravitational acceleration Y (m/s^2)
+  data.gravityZ = event.acceleration.z;     // Gravitational acceleration Z (m/s^2)
 
   return data;
 }
 //---------------------------------------- BNO data read types sending end -------------------------------------------//
 
 //---------------------------------------- checksum calculation for xbee data sending ------------------------------------------------//
-void check(unsigned int len){
- 
+void check(unsigned int len)
+{
+
   int sum = 0;
-  for (unsigned int i = 3; i <  len-1+17; i++) {
+  for (unsigned int i = 3; i < len - 1 + 17; i++)
+  {
 
     sum += packet[i];
-
   }
   byte checksum = 0;
-
 
   checksum += (sum % 16);
   sum = (sum / 16);
   checksum += (sum % 16) * 16;
   checksum = 255 - checksum;
-  packet[len-1+17] = checksum;
+  packet[len - 1 + 17] = checksum;
 }
 //---------------------------------------- checksum calculation for xbee data end ----------------------------------------------------//
 
-
-void setup() {
+void setup()
+{
   // Initialize both serial ports:
   Serial.begin(57600);
-  Serial1.begin(9600);//GPS connection
-  Serial2.begin(9600);//Xbee connection serial
+  Serial1.begin(9600); // GPS connection
+  Serial2.begin(9600); // Xbee connection serial
   // Initialize BNO055 sensor
-  if (!bno.begin()) {
+  if (!bno.begin())
+  {
     Serial.println("BNO055 not detected. Please check wiring or I2C address.");
-    while(1);
+    while (1)
+      ;
   }
   starxbee = millis();
   starbno = millis();
 
-// Initialize BMP388 sensor
-  if (!bmp.begin_I2C(0x76,&Wire)) {
+  // Initialize BMP388 sensor
+  if (!bmp.begin_I2C(0x76, &Wire))
+  {
     Serial.println("Could not find a valid BMP3 sensor, check wiring!");
-    while (1);
+    while (1)
+      ;
   }
-  
+
   bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_8X);
   bmp.setPressureOversampling(BMP3_OVERSAMPLING_4X);
   bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
   bmp.setOutputDataRate(BMP3_ODR_50_HZ);
 }
 
-void loop() {
-
+void loop()
+{
+  //----------------------------------------  data Parse for GPS  -------------------------------------------------//
   unsigned long starttime = millis();
-  while((millis() - starttime) <=700) // do this loop for up to 1000mS
+  while ((millis() - starttime) <= 700) // do this loop for up to 1000mS
   {
-      if (Serial1.available()) {
+    if (Serial1.available())
+    {
       char c = Serial1.read();
-  ////    Serial.write(c); // Uncomment this line if you want to see the GPS data flowing
+      ////    Serial.write(c); // Uncomment this line if you want to see the GPS data flowing
       gps.encode(c);
     }
   }
+  //----------------------------------------  data Parse for GPS end  ----------------------------------------------//
 
-  
-//----------------------------------------  data read for GPS,BNO,etc  -------------------------------------------------//
+  //----------------------------------------  data read for GPS,BNO,etc  -------------------------------------------------//
   struct GNSSData gnssData = readGNSSData(gps);
   struct VoltageData voltageData = readVoltageSensor();
   struct VibrationData vibrationData = readVibrationSensor();
- 
+
   struct BNO055Data bnoData = readBNO055Sensor();
-//----------------------------------------  data read for GPS,BNO,etc ends ---------------------------------------------//
+  //----------------------------------------  data read for GPS,BNO,etc ends ---------------------------------------------//
   // Now, you have the GNSS data in gnssData, the voltage data in voltageData,
   // the vibration data in vibrationData, and the BNO055 sensor data in bnoData structs.
   // You can use these data as needed or store them in variables.
   // For example:
 
- //---------------------------------------- Making string of data for transmission procedure ---------------------------------------------//
-  String data ="";//string containing the data
- 
+  //---------------------------------------- Making string of data for transmission procedure ---------------------------------------------//
+  String data = ""; // string containing the data
+
   float voltage = voltageData.voltage;
   int vibration = vibrationData.vibration;
 
-  
-  data += String(gnssData.time)+" ";
-  data += String(n) +" "+data;
-  data += String(gnssData.speed , 2) + " ";
-  data += String(gnssData.latitude , 2) + " ";
-  data += String(gnssData.longitude , 2) + " ";
-  data += String(gnssData.altitude , 2) + " ";
-  data += String(gnssData.satellites , 2) + " ";
-  data += String(voltage , 2) + " ";
-  data += String(vibration , 2) + " ";
-  data += String(bnoData.eulerX , 2) + " ";
-  data += String(bnoData.eulerY , 2) + " ";
-  data += String(bnoData.eulerZ , 2) + " ";
-  data += String(bnoData.accelX , 2) + " ";
-  data += String(bnoData.accelY , 2) + " ";
-  data += String(bnoData.accelZ , 2) + " ";
-  data += String(bnoData.gyroX , 2) + " ";
-  data += String(bnoData.gyroY , 2) + " ";
-  data += String(bnoData.gyroZ , 2) + " ";
-  data += String(bnoData.temperature , 2) + " ";
-  data += String(bnoData.magX , 2) + " ";
-  data += String(bnoData.magY , 2) + " ";
-  data += String(bnoData.magZ , 2) + " ";
-  data += String(bnoData.linearAccelX , 2) + " ";
-  data += String(bnoData.linearAccelY , 2) + " ";
-  data += String(bnoData.linearAccelZ , 2) + " ";
-  data += String(bnoData.gravityX , 2) + " ";
-  data += String(bnoData.gravityY , 2) + " ";
-  data += String(bnoData.gravityZ , 2) + " ";
-  data += String(bmp.readAltitude(SEALEVELPRESSURE_HPA) , 2) + " ";
-  data += String(bmp.temperature , 2) + " ";
-  data += String(bmp.pressure / 100.0,2)+" ";
- 
+  data += String(gnssData.time) + ",";
+  data += String(n) + ",";
+  data += String(gnssData.speed, 2) + ",";
+  data += String(gnssData.latitude, 7) + ",";
+  data += String(gnssData.longitude, 7) + ",";
+  data += String(gnssData.altitude, 2) + ",";
+  data += String(gnssData.satellites, 2) + ",";
+  data += String(voltage, 2) + ",";
+  data += String(vibration, 2) + ",";
+  data += String(bnoData.eulerX, 2) + ",";
+  data += String(bnoData.eulerY, 2) + ",";
+  data += String(bnoData.eulerZ, 2) + ",";
+  data += String(bnoData.accelX, 2) + ",";
+  data += String(bnoData.accelY, 2) + ",";
+  data += String(bnoData.accelZ, 2) + ",";
+  data += String(bnoData.gyroX, 2) + ",";
+  data += String(bnoData.gyroY, 2) + ",";
+  data += String(bnoData.gyroZ, 2) + ",";
+  data += String(bnoData.temperature, 2) + ",";
+  data += String(bnoData.magX, 2) + ",";
+  data += String(bnoData.magY, 2) + ",";
+  data += String(bnoData.magZ, 2) + ",";
+  data += String(bnoData.linearAccelX, 2) + ",";
+  data += String(bnoData.linearAccelY, 2) + ",";
+  data += String(bnoData.linearAccelZ, 2) + ",";
+  data += String(bnoData.gravityX, 2) + ",";
+  data += String(bnoData.gravityY, 2) + ",";
+  data += String(bnoData.gravityZ, 2) + ",";
+  data += String(bmp.readAltitude(SEALEVELPRESSURE_HPA), 2) + ",";
+  data += String(bmp.temperature, 2) + ",";
+  data += String(bmp.pressure / 100.0, 2) + ",";
+
   Serial.println(data);
   Serial.println(data.length());
-  
- //---------------------------------------- Making string of data for transmission procedure ends -----------------------------------------//
 
- 
+  //---------------------------------------- Making string of data for transmission procedure ends -----------------------------------------//
+
   //---------------------------------------- Xbee data transmission procedure starts ---------------------------------------------//
   byte datasend[2500];
   data.getBytes(datasend, data.length());
-  for(unsigned int i = 0 ; i< data.length()-1;i++){
-    packet[17+i] = datasend[i];
+  for (unsigned int i = 0; i < data.length() - 1; i++)
+  {
+    packet[17 + i] = datasend[i];
   }
   // length of data start
-  packet[1] = (14+data.length()-1)/256;
-  packet[2] = (14+data.length()-1)%256;
+  packet[1] = (14 + data.length() - 1) / 256;
+  packet[2] = (14 + data.length() - 1) % 256;
   // length of data end
   check(data.length());
-//  for (unsigned int i =  0 ; i< data.length()+17 ; i++){
-//    Serial.print(packet[i]);
-//    Serial.print(" ");
-//  }
-//  Serial.println();
-  if(millis() - starxbee > 1000){
-  Serial2.write(packet,data.length()+17);
-  starxbee = millis();
-  n++;
+  //  for (unsigned int i =  0 ; i< data.length()+17 ; i++){
+  //    Serial.print(packet[i]);
+  //    Serial.print(" ");
+  //  }
+  //  Serial.println();
+  if (millis() - starxbee > 1000)
+  {
+    Serial2.write(packet, data.length() + 17);
+    starxbee = millis();
+    n++;
   }
   //---------------------------------------- Xbee data transmission procedure ends ---------------------------------------------//
   // You can perform further actions with the data here
